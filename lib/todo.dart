7efc0d1ba243706d349eedb800c653025899aa6e
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-03-16 23:08:12 +0600

    added delete button

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class todo extends StatefulWidget {
   const todo({super.key});
@@ -171,3 +171,403 @@ class MyTodo {
 }
 
 enum todoPriority { Low, Normal, High }
+*/
+/*import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  const Todo({Key? key});
+
+  @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  final _controller = TextEditingController();
+  TodoPriority priority = TodoPriority.Normal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Tasks'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addTodo();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: MyTodo.todos.isEmpty
+          ? Center(
+        child: Text('Nothing To do!'),
+      )
+          : ListView.builder(
+        itemCount: MyTodo.todos.length,
+        itemBuilder: (context, index) {
+          final todo = MyTodo.todos[index];
+          return TodoItem(
+            todo: todo,
+            onChanged: (value) {
+              setState(() {
+                MyTodo.todos[index].completed = value;
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void addTodo() {
+    showModalBottomSheet(
+
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setBuilderState) => Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'What to do?',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('Priority'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Radio<TodoPriority>(
+                    value: TodoPriority.Low,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.Low.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.Normal,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.Normal.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.High,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.High.name),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: _save,
+                child: Text('Save'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _save() {
+    if (_controller.text.isEmpty) {
+      showMsg(context, 'Input must not be empty');
+      return;
+    }
+    final todo = MyTodo(
+      id: DateTime.now().millisecondsSinceEpoch,
+      name: _controller.text,
+      priority: priority,
+    );
+    MyTodo.todos.add(todo);
+    _controller.clear();
+    setState(() {});
+    Navigator.pop(context);
+  }
+}
+
+void showMsg(BuildContext context, String s) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Caution!'),
+      content: Text(s),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('Close'),
+        ),
+      ],
+    ),
+  );
+}
+
+class TodoItem extends StatelessWidget {
+  final MyTodo todo;
+  final Function(bool) onChanged;
+
+  const TodoItem({Key? key, required this.todo, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: CheckboxListTile(
+        title: Text(
+          todo.name,
+          style: TextStyle(
+            decoration: todo.completed ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        subtitle: Text('Priority: ${todo.priority.name}'),
+        value: todo.completed,
+          onChanged: (value) {
+            onChanged(value!);
+          }
+      ),
+    );
+  }
+}
+
+class MyTodo {
+  int id;
+  String name;
+  bool completed;
+  TodoPriority priority;
+
+  MyTodo({
+    required this.id,
+    required this.name,
+    this.completed = false,
+    required this.priority,
+  });
+
+  static List<MyTodo> todos = [];
+}
+
+enum TodoPriority { Low, Normal, High }
+
+*/
+import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  const Todo({Key? key});
+
+  @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  final _controller = TextEditingController();
+  TodoPriority priority = TodoPriority.Normal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Tasks'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addTodo();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: MyTodo.todos.isEmpty
+          ? Center(
+        child: Text('Nothing To do!'),
+      )
+          : ListView.builder(
+        itemCount: MyTodo.todos.length,
+        itemBuilder: (context, index) {
+          final todo = MyTodo.todos[index];
+          return TodoItem(
+            todo: todo,
+            onChanged: (value) {
+              setState(() {
+                MyTodo.todos[index].completed = value;
+              });
+            },
+            onDelete: () {
+              setState(() {
+                MyTodo.todos.removeAt(index);
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void addTodo() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setBuilderState) => Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'What to do?',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('Priority'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Radio<TodoPriority>(
+                    value: TodoPriority.Low,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.Low.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.Normal,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.Normal.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.High,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.High.name),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: _save,
+                child: Text('Save'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _save() {
+    if (_controller.text.isEmpty) {
+      showMsg(context, 'Input must not be empty');
+      return;
+    }
+    final todo = MyTodo(
+      id: DateTime.now().millisecondsSinceEpoch,
+      name: _controller.text,
+      priority: priority,
+    );
+    MyTodo.todos.add(todo);
+    _controller.clear();
+    setState(() {});
+    Navigator.pop(context);
+  }
+}
+
+void showMsg(BuildContext context, String s) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Caution!'),
+      content: Text(s),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('Close'),
+        ),
+      ],
+    ),
+  );
+}
+
+class TodoItem extends StatelessWidget {
+  final MyTodo todo;
+  final Function(bool) onChanged;
+  final VoidCallback onDelete;
+
+  const TodoItem({
+    Key? key,
+    required this.todo,
+    required this.onChanged,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          todo.name,
+          style: TextStyle(
+            decoration: todo.completed ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        subtitle: Text('Priority: ${todo.priority.name}'),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: onDelete,
+        ),
+        leading: Checkbox(
+          value: todo.completed,
+          onChanged: (value) {
+            onChanged(value!);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class MyTodo {
+  int id;
+  String name;
+  bool completed;
+  TodoPriority priority;
+
+  MyTodo({
+    required this.id,
+    required this.name,
+    this.completed = false,
+    required this.priority,
+  });
+
+  static List<MyTodo> todos = [];
+}
+
+enum TodoPriority { Low, Normal, High }
+
